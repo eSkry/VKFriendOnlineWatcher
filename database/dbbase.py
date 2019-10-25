6cc0a16acd38ca68e7f4a245a604dc69b5850b0d
Depish <reactor.game@gmail.com>
2019-10-25 08:53:54 +0300

    add abstract tags

--- database/dbbase.py
+++ database/dbbase.py
@@ -1,24 +1,26 @@
-import os
+from abc import ABCMeta, abstractmethod, abstractproperty
 
-
-class DbBase:
-    def __init__(self):
-        pass
-
+class DbBase:    
+    @abstractmethod
     def SetUserOnline(self, user_id, timestamp):
         pass
     
+    @abstractmethod
     def SetUserOffline(self, user_id, timestamp):
         pass
 
+    @abstractmethod
     def GetUserLastState(self, user_id):
         pass
 
+    @abstractmethod
     def IsUSerExists(self, user_id):
         pass
-
+    
+    @abstractmethod
     def AddUser(self, user_id, full_name):
         pass
-
+    
+    @abstractmethod
     def GetConnection(self):
         pass
