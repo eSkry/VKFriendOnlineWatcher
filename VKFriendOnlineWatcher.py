0899f485b86b84e87865011e8f475866038e6788
Depish <reactor.game@gmail.com>
2019-10-25 08:27:01 +0300

    remove old code

--- VKFriendOnlineWatcher.py
+++ VKFriendOnlineWatcher.py
@@ -39,14 +39,48 @@ class Main(object):
             tags = { 'user': event.user_id, 'full_name': event.full_name, 'platform': event.last_seen }
             if event.type == VkEventType.USER_ONLINE:
                 db.InsertOnline(self.DB, event.user_id, event.timestamp)
-                pgt_sender.AddToPool(pgt_sender.GetMetricsStr('friends_online_stats', tags, '1'))
+                self.pgt_sender.AddToPool(self.pgt_sender.GetMetricsStr('friends_online_stats', tags, '1'))
             elif event.type == VkEventType.USER_OFFLINE:
                 db.InsertOffline(self.DB, event.user_id, event.timestamp, event.last_seen)
+                self.pgt_sender.AddToPool(self.pgt_sender.GetMetricsStr('friends_online_stats', tags, '0'))
             
             if event.user_id not in self.VK_USER_IDS:
                 self.VK_USER_IDS.append(event.user_id)
                 db.AddNewUser(self.DB, event.user_id, event.full_name)
 
+    def UpdateDopUsers(self):
+        dop_users = self.vkapi.users.get(user_uds=self.DOP_USER_IDS, fields=['online', 'last_seen'])
+        timestamp = self.GetUnixTimestamp()
+        for user in dop_users:
+            user_id = int(user['id'])
+            full_name = '{} {}'.format(user['first_name'], user['last_name'])
+            user_online = int(user['online'])
+            user_last_seen = -1
+
+            if 'last_seen' in user:
+                user_last_seen = int(user['last_seen']['platform'])
+
+            if not user_id in self.VK_USER_IDS:
+                self.VK_USER_IDS.append(user_id)
+                if not db.IsUserExists(self.DB, user_id):
+                    db.AddNewUser(self.DB, user_id, full_name, False)
+
+            state = db.GetLastState(self.DB, user_id)
+            if state != None:
+                if self.CONFIG.PROMETHEUS_SEND:
+                    tags = { 'user': user_id, 'full_name': full_name, 'platform': user_last_seen }
+                    self.pgt_sender.AddToPool(self.pgt_sender.GetMetricsStr('friends_online_stats', tags, str(user_online)))
+                if int(state) == user_online:
+                    continue
+
+            if user_online == 0:
+                db.InsertOffline(self.DB, user_id, timestamp, user_last_seen, False)
+            elif user_online == 1:
+                db.InsertOnline(self.DB, user_id, timestamp, False)
+
+        self.DB.commit()
+        if self.CONFIG.PROMETHEUS_SEND:
+            self.pgt_sender.SendFromPool()
 
     def _updateDopUsers(self):
         if self.CONFIG.HAS_UPSER_FILE:
@@ -60,59 +94,5 @@ class Main(object):
 
 
 
-
-def update_metrics(vk, conn):
-    friends = vk.friends.get(fields=['online', 'last_seen'])['items']
-    dop_users = vk.users.get(user_ids=DOP_USER_IDS, fields=['online', 'last_seen'])
-    user_status_list = friends + dop_users
-
-    for user in user_status_list:
-        user_id = int(user['id'])
-        user_online = int(user['online'])
-        user_last_seen = -1
-        if 'last_seen' in user:
-            user_last_seen = int(user['last_seen']['platform'])
-
-        if not user_id in VK_USER_IDS:
-            VK_USER_IDS.append(user_id)
-            if not db.IsUserExists(conn, user_id):
-                db.AddNewUser(conn, user_id, full_name, False)
-
-        if state != None:
-            if PUSHGATWAY_SEND:
-                tags = { 'user': user_id, 'full_name': full_name, 'platform': user_last_seen }
-                pgt_sender.AddToPool( pgt_sender.GetMetricsStr('friends_online_stats', tags, str(user_online)) )
-            if int(state) == user_online:
-                continue
-
-        if user_online == 0:
-            db.InsertOffline2(conn, user_id, timestamp, user_last_seen, False)
-        elif user_online == 1:
-            db.InsertOnline2(conn, user_id, timestamp, False)
-
-    conn.commit()
-    
-    if PUSHGATWAY_SEND:
-        try:
-            pgt_sender.SendFromPool()
-        except Exception as e:
-            print('Pushgateway send error')
-            print(e)
-        finally:
-            pass
-
-
-KILL_APP = False
-
-if CONFIG_PATH == None:
-    print('Config is not exists! Please create config cin ./config/config.conf')
-    sys.exit(0)
-
-print('Using config file: {}'.format(CONFIG_PATH))
-conn = db.CreateDB('./sql/init.sql')
-
-while not KILL_APP:
-    update_metrics(vk, conn)
-    time.sleep(UPDATE_TIME)
-
-print('Stop working')
+if __name__ == '__main__':
+    app = Main()
